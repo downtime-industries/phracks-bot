02588f3299dc074957cfc56d70684e1fa86df57f
David Tippett <dtaivpp@gmail.com>
2024-03-09 15:25:20 -0500

    adding framing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # phracks-bot
 An interface into the Phracks archive. 
+
+docker compose up -d
+
+"python3 -m pip install -r requirements.txt"
---
+++ src/ingestion.py
@@ -0,0 +1,7 @@
+# Create document store EG OpenSearch: 
+# https://docs.haystack.deepset.ai/v2.0/docs/opensearch-document-store
+
+# Create preprocessing pipeline (chunks the data)
+# https://docs.haystack.deepset.ai/v2.0/docs/documentsplitter
+
+# fin
